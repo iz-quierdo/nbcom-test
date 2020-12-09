865a3901f973677ef011f7a55afe1a5e5c3cfb1b
Alexander Smirnov <alex-wd@yandex.ru>
2020-12-09 19:47:30 +0300

    Interface updates

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,105 @@
 import Vue from 'vue/dist/vue.min';
 import axios from 'axios/dist/axios.min';
 
+Vue.component('tabs', {
+  template: `
+      <div>
+          <div class="tabs">
+            <ul>
+              <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                  <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="tabs-details">
+              <slot></slot>
+          </div>
+      </div>
+  `,
+
+  data() {
+    return { tabs: [] };
+  },
+
+  created() {
+    this.tabs = this.$children;
+  },
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach((tab) => {
+        tab.isActive = tab.name == selectedTab.name;
+      });
+    },
+  },
+});
+
+Vue.component('tab', {
+  template: `
+      <div v-show="isActive"><slot></slot></div>
+  `,
+
+  props: {
+    name: { required: true },
+    selected: { default: false },
+  },
+
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+
+  computed: {
+    href() {
+      return '#' + this.name.toLowerCase().replace(/ /g, '-');
+    },
+  },
+
+  mounted() {
+    this.isActive = this.selected;
+  },
+});
+
+Vue.component('option-block', {
+  props: ['title', 'code', 'config'],
+  template: `
+  <div class="option-block" :class="{ active: isActive }">
+    <h3 @click="toggleMe"><span>{{ title }}</span></h3>
+      <config-block
+        v-for="configItem in config"
+        v-bind:code="code"
+        v-bind:id="configItem.id"
+        v-bind:text="configItem.text"
+        v-bind:extend="configItem.extend"
+        v-bind:bundle="configItem.bundle"
+        v-bind:add="configItem.add"
+      ></config-block>
+  </div>
+  `,
+
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+
+  methods: {
+    toggleMe() {
+      this.isActive = !this.isActive;
+    },
+  },
+});
+
 Vue.component('config-block', {
   props: ['code', 'id', 'text', 'extend', 'bundle', 'add'],
   template: `
-	<div class="config-block">
-		<input class="config-block__input" :disabled="isDisabled" type="radio" :name="code" :id="code + id">
-		<label class="config-block__label" :disabled="isDisabled" :for="code + id">{{ text }}</label>
-		<p class="config-block__extend" v-if="extend">{{ extend }}</p>
-	</div>
-	`,
+  <div class="config-block">
+    <input class="config-block__input" :disabled="isDisabled" type="radio" :name="code" :id="code + id">
+    <label class="config-block__label" :disabled="isDisabled" :for="code + id">{{ text }}</label>
+    <p class="config-block__extend" v-if="extend">{{ extend }}</p>
+  </div>
+  `,
   computed: {
     isDisabled() {
       if (this.add === undefined) {
@@ -21,30 +111,27 @@ Vue.component('config-block', {
   },
 });
 
-Vue.component('option-block', {
-  props: ['title', 'code', 'config'],
+Vue.component('option-result', {
+  props: ['title'],
   template: `
-	<div>
-		<h3>{{ title }}</h3>
-		<fieldset :id="code">
-			<config-block
-				v-for="configItem in config"
-				v-bind:code="code"
-				v-bind:id="configItem.id"
-				v-bind:text="configItem.text"
-				v-bind:extend="configItem.extend"
-				v-bind:bundle="configItem.bundle"
-				v-bind:add="configItem.add"
-			></config-block>
-		</fieldset>
-	</div>
-	`,
+  <div class="option-result">
+    <h3>Выбранный тип устройства</h3>
+    {{ getTitle }}
+  </div>
+  `,
+  computed: {
+    getTitle() {
+      return this.title;
+    },
+  },
 });
 
 new Vue({
   el: '#app',
-  data: {
-    options: [],
+  data() {
+    return {
+      options: [],
+    };
   },
   mounted() {
     axios.get('http://localhost:3000/data.json').then((response) => {
@@ -64,6 +151,9 @@ new Vue({
       return this.options.filter(
         (option) => option.config[0].bundle !== undefined
       );
+    },
+    typeOptionsTitle() {
+      return 'test';
     },
   },
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
-##Nb company test assignment
+# Nb company test assignment
 
-Install
+This app is recommended to use with Node.js 12 or 14
+Other versions was not tested
+
+Install node modules with all dependencies
+
+> npm install
+
+Running gulp live server
+
+> gulp
