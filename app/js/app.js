19ba77bf55c33185a440bf0cbdd5cb56a4cf67b7
Alexander Smirnov <alex-wd@yandex.ru>
2020-12-10 11:15:13 +0300

    Result component corrections

--- app/js/app.js
+++ app/js/app.js
@@ -65,16 +65,12 @@ Vue.component('option-block', {
   props: ['title', 'code', 'config'],
   template: `
   <div class="option-block" :class="{ active: isActive }">
-    <h3 @click="toggleMe"><span>{{ title }}</span></h3>
-      <config-block
-        v-for="configItem in config"
-        v-bind:code="code"
-        v-bind:id="configItem.id"
-        v-bind:text="configItem.text"
-        v-bind:extend="configItem.extend"
-        v-bind:bundle="configItem.bundle"
-        v-bind:add="configItem.add"
-      ></config-block>
+    <h3 @click="toggle"><span>{{ title }}</span></h3>
+      <div class="config-block" v-for="configItem in config">
+        <input v-on:change="emitSelectEvent" class="config-block__input" :disabled="configItem.add === false" :value="configItem.text" type="radio" :name="code" :id="code + configItem.id">
+        <label class="config-block__label" :disabled="configItem.add === false" :for="code + configItem.id">{{ configItem.text }}</label>
+        <p class="config-block__extend" v-if="configItem.extend">{{ configItem.extend }}</p>
+      </div>
   </div>
   `,
 
@@ -85,43 +81,26 @@ Vue.component('option-block', {
   },
 
   methods: {
-    toggleMe() {
+    toggle() {
       this.isActive = !this.isActive;
     },
-  },
-});
-
-Vue.component('config-block', {
-  props: ['code', 'id', 'text', 'extend', 'bundle', 'add'],
-  template: `
-  <div class="config-block">
-    <input class="config-block__input" :disabled="isDisabled" type="radio" :name="code" :id="code + id">
-    <label class="config-block__label" :disabled="isDisabled" :for="code + id">{{ text }}</label>
-    <p class="config-block__extend" v-if="extend">{{ extend }}</p>
-  </div>
-  `,
-  computed: {
-    isDisabled() {
-      if (this.add === undefined) {
-        return false;
-      } else {
-        return !this.add;
-      }
+    emitSelectEvent() {
+      this.$emit('selected');
     },
   },
 });
 
 Vue.component('option-result', {
-  props: ['title'],
+  props: ['text'],
   template: `
   <div class="option-result">
-    <h3>Выбранный тип устройства</h3>
-    {{ getTitle }}
+    {{ getText }}
   </div>
   `,
+
   computed: {
-    getTitle() {
-      return this.title;
+    getText() {
+      return this.text;
     },
   },
 });
@@ -131,12 +110,28 @@ new Vue({
   data() {
     return {
       options: [],
+      resultBlock: '',
     };
   },
   mounted() {
     axios.get('http://localhost:3000/data.json').then((response) => {
       this.options = response.data;
     });
+  },
+  methods: {
+    onSelectButton() {
+      console.log('this.resultBlock');
+      const typeHeader = document.querySelector('#type-block h3').textContent;
+      const typeRadios = document.querySelector(
+        '#type-block input[type=radio]:checked'
+      ).value;
+      console.log(typeRadios);
+      this.resultBlock = `
+        <h2>${typeHeader}</h2>
+        <p>${typeRadios}</p>
+
+      `;
+    },
   },
   computed: {
     typeOptions() {
@@ -151,9 +146,6 @@ new Vue({
       return this.options.filter(
         (option) => option.config[0].bundle !== undefined
       );
-    },
-    typeOptionsTitle() {
-      return 'test';
     },
   },
 });
